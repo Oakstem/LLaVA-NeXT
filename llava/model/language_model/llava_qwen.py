533c210951f9848b2f06a946f0cfc65859e93362
Alon <alonmardi@mail.tau.ac.il>
2025-10-01 11:42:40 +0300

    Refactor forward method to enhance flexibility with additional kwargs and clean up commented code

--- llava/model/language_model/llava_qwen.py
+++ llava/model/language_model/llava_qwen.py
@@ -77,11 +77,15 @@ class LlavaQwenForCausalLM(Qwen2ForCausalLM, LlavaMetaForCausalLM):
         modalities: Optional[List[str]] = ["image"],
         dpo_forward: Optional[bool] = False,
         cache_position=None,
+        # focus_loss_after_phrase: Optional[bool] = None,
+        # focus_loss_phrase_token_ids: Optional[Union[torch.Tensor, List[List[int]]]] = None,
+        # focus_loss_missing_value: Optional[float] = None,
+        **kwargs,
     ) -> Union[Tuple, CausalLMOutputWithPast]:
 
-        # if images is not None:
-        #     for i, image in enumerate(images):
-        #         images[i] = image.to(self.dtype)
+        if images is not None:
+            for i, image in enumerate(images):
+                images[i] = image.to(self.dtype)
 
         if inputs_embeds is None:
             (input_ids, position_ids, attention_mask, past_key_values, inputs_embeds, labels) = self.prepare_inputs_labels_for_multimodal(input_ids, position_ids, attention_mask, past_key_values, labels, images, modalities, image_sizes)
@@ -115,6 +119,10 @@ class LlavaQwenForCausalLM(Qwen2ForCausalLM, LlavaMetaForCausalLM):
                 output_attentions=output_attentions,
                 output_hidden_states=output_hidden_states,
                 return_dict=return_dict,
+                # focus_loss_after_phrase=focus_loss_after_phrase,
+                # focus_loss_phrase_token_ids=focus_loss_phrase_token_ids,
+                # focus_loss_missing_value=focus_loss_missing_value,
+                **kwargs,
             )
 
     @torch.no_grad()
